8683c2be6f9090a06999917d6b4cf7e18744d0a4
cmce2019 <carlosm.cordoba1@correo.usa.edu.co>
2020-02-16 18:13:24 -0500

    CV web Carlos Cordoba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { PagerrorComponent } from './pagerror/pagerror.component';
 import {DataService} from './data.service';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
   { path: 'firstcomp', component: FirstComponentComponent },
   { path: 'PagerrorComponent', component: PagerrorComponent },
-  { path: '', component: FirstComponentComponent, pathMatch: 'full' },
+  { path: '', component: ContentComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '/PagerrorComponent', pathMatch: 'full' }
 ];
 
@@ -22,13 +26,17 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    PagerrorComponent
+    PagerrorComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
     HttpClientModule,
+    AngularFontAwesomeModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDSPZc4rUlbyfLM4SLHe2c0iu8e-2TGGFA'
     }),
